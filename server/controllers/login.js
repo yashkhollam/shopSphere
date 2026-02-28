ddb45a6c17efe3047c6ced1ecabda23d75215937
yashkhollam <yashkhollam01@gmail.com>
2026-02-28 11:56:56 +0530

    fix some bugs

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -51,9 +51,9 @@ export const login=async(req,res)=>{
             res.cookie('JWTTOKEN',token,{
                 httpOnly:true,
                 //    secure:process.env.NODE_ENV==="production", //true in production
-                //   sameSite:"lax",//localhost practise
+                //   sameSite:"lax", //localhost practise
               
-                sameSite:"none",  //none in production
+                sameSite:"none", //none in production
                 secure:true,      
                 
                 maxAge:24*60*60*1000
